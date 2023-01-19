43e7d9c294f4d04c7dff31cfc8b349b806f162ff
João Victor Silva <victor.joao.1611@gmail.com>
2023-01-19 15:38:29 -0300

    refactor: use unsafe read

--- audio/callbacks.go
+++ audio/callbacks.go
@@ -34,7 +34,7 @@ func readBuffer(userdataPointer unsafe.Pointer, stream *C.Uint8, length C.int) {
 
 	streamSlice := CPoiterToSlice(stream, length)
 	for i := 0; i + 3 < int(length); i = i + 4 {
-		data := audioDevice.readData()
+		data := audioDevice.ReadDataUnsafe()
 		binaryData := make([]byte, 4)
 		binary.LittleEndian.PutUint32(binaryData, math.Float32bits(data))
 		streamSlice[i] = (C.Uint8) (binaryData[0])
