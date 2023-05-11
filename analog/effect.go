9e5ee5229ba998befe3bd94db8343a5491a1d8ed
João Victor Silva <victor.joao.1611@gmail.com>
2023-05-11 09:58:33 -0300

    feat: add dummy pedal and effect

--- analog/effect.go
+++ analog/effect.go
@@ -3,3 +3,10 @@ package analog
 type Effect interface {
 	Process([]Signal) []Signal
 }
+
+type DummyEffect struct {
+}
+
+func (e *DummyEffect) Process(signals []Signal) []Signal {
+	return signals
+}
--- analog/pedal.go
+++ analog/pedal.go
@@ -5,3 +5,27 @@ type Pedal interface {
 	GetOutputJack() []OutputJack
 	Toggle()
 }
+
+func NewPedal(effect Effect) Pedal {
+	return &BasePedal{
+		effect: effect,
+		inputs: make([]InputJack, 1),
+		outputs: make([]OutputJack, 1),
+	}
+}
+
+func NewDummyPedal() Pedal {
+	return &BasePedal{
+		effect: &DummyEffect{},
+		inputs: make([]InputJack, 1),
+		outputs: make([]OutputJack, 1),
+	}
+}
+
+type BasePedal struct {
+	effect Effect
+	inputs []InputJack
+	outputs []OutputJack
+	isOn bool
+}
+
