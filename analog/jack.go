39e53b2c418cfe3358a519f50ce00889cadcfe28
João Victor Silva <victor.joao.1611@gmail.com>
2023-05-11 09:58:52 -0300

    feat: add base jack interface

--- analog/jack.go
+++ analog/jack.go
@@ -4,12 +4,32 @@ type jack struct {
 	wire Wire
 }
 
+type Jack interface {
+	Connect(wire Wire)
+}
+
 type InputJack interface {
+	Jack
 	ReceiveSignal() Signal
 }
 
 type OutputJack interface {
+	Jack
 	SendSignal(Signal)
+}
+
+// type InputOutputJack interface {
+// 	InputJack
+// 	OutputJack
+// }
+
+func (j *jack) Connect(wire Wire) {
+	_, open := <- j.wire
+	if open {
+		close(j.wire)
+	}
+
+	j.wire = wire
 }
 
 func (j *jack) ReceiveSignal() Signal {
