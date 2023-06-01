bb829e3d7e78c9752395775579e67837221a585e
João Victor Silva <victor.joao.1611@gmail.com>
2023-06-01 09:42:28 -0300

    feat: add buffered functions to send and receive signals

--- analog/jack.go
+++ analog/jack.go
@@ -6,16 +6,19 @@ type jack struct {
 
 type Jack interface {
 	Connect(wire Wire)
+	GetWire() Wire
 }
 
 type InputJack interface {
 	Jack
 	ReceiveSignal() Signal
+	BufferedReceiveSignal(size int) []Signal
 }
 
 type OutputJack interface {
 	Jack
 	SendSignal(Signal)
+	BufferedSendSignal([]Signal)
 }
 
 // type InputOutputJack interface {
@@ -37,13 +40,38 @@ func (j *jack) Connect(wire Wire) {
 	j.wire = wire
 }
 
+func (j *jack) GetWire() Wire {
+	return j.wire
+}
+
 func (j *jack) ReceiveSignal() Signal {
 	return <-j.wire
+}
+
+func (j *jack) BufferedReceiveSignal(size int) []Signal {
+	result := make([]Signal, 0, size)
+	for i := 0; i < size; i++ {
+		var value Signal
+		select {
+		case value = <-j.wire:
+		default:
+		}
+		result = append(result, value)
+	}
+	return result
 }
 
 func (j *jack) SendSignal(signal Signal) {
 	j.wire <- signal
 }
+
+
+func (j *jack) BufferedSendSignal(signals []Signal) {
+	for _, signal := range signals {
+		j.wire <- signal
+	}
+}
+
 
 func NewInputJack() InputJack {
 	return &jack{wire: make(Wire)}
